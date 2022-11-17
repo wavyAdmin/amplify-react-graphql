5cfa7f0642573d5af0e94aeeecdeb736910120f7
RevnYou-Dapp <info@wavy.network>
2022-11-17 09:49:49 -0500

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s32fa19085": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
